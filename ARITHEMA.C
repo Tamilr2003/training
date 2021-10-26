3905934f890842caaccbe67dce3f27ca684f1699
Tamilr2003 <93188440+Tamilr2003@users.noreply.github.com>
2021-10-26 15:16:39 +0530

    Add files via upload

---
+++ ARITHEMA.C
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+ #include<conio.h>
+ int main()
+  {
+ int a=100;
+ int b=2;
+ int result;
+ result=a-b;
+ clrscr();
+ printf("a-b=%i/n",result);
+ result=a*b;
+ printf("a*b=%i/n",result);
+ result=a/b;
+ printf("a/b=%i/n",result);
+ result=a+b;
+ printf("a+b=%i/n",result);
+ getch();
+ return 0;
+ }
